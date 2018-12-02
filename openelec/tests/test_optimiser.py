ef4f572ddedcd2820c8727d147ac352e89247473
Chris Arderne <chris@rdrn.me>
2018-12-02 15:14:41 +0100

    Fix tests

--- openelec/tests/test_optimiser.py
+++ openelec/tests/test_optimiser.py
@@ -1,4 +1,4 @@
-from openelec import mgo
+from openelec import util
 from pathlib import Path
 import geopandas as gpd
 from collections import defaultdict
@@ -8,5 +8,5 @@ TEST_DATA = Path('test_data')
 BUILDINGS = TEST_DATA / 'Nakiu.geojson'
 
 def test_clip_raster():
-	villages = mgo.village_centroids(TEST_DATA)
+	villages = util.village_centroids(TEST_DATA)
 	assert isinstance(villages, defaultdict)
